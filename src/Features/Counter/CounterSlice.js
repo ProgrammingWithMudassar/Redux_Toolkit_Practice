39e13fb24c39264de388d5738d5263d851bb2338
Mudassar Hussain <mudassarhus667788@gmail.com>
2023-04-30 23:43:52 +0500

    Code Updated

--- src/Features/Counter/CounterSlice.js
+++ src/Features/Counter/CounterSlice.js
@@ -10,14 +10,14 @@ export const counterSlice = createSlice({
   initialState,
   reducers: {
     increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
+      
       state.value += 1
     },
     decrement: (state) => {
-      state.value -= 1
+      let StateValue = state.value;
+      {
+      StateValue>=1  ? state.value -= 1  : alert("You can't reduce the number more.")
+      }
     },
     incrementByAmount: (state, action) => {
       state.value += action.payload
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,36 +1,75 @@
 import React from 'react'
 import {
-  Box, Button
+  Box, Button, Typography
 } from '@mui/material'
 import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment } from '../Features/Counter/CounterSlice'
-
+import { SecPlus, SecMinus } from '../Features/Counter/SecondarySlice'
+import { minus, add } from '../Features/Counter/ThirdSlice'
 
 
 const Home = () => {
-
   const count = useSelector((state) => {
-    console.log(state);
-    return (
-      state.counter
-    )
+    return ({
+      firstCounter: state.counter,
+      secondCounter: state.SecondCounter,
+      thirdCounter: state.ThirdCounter,
+    })
   })
   const dispatch = useDispatch();
 
+//First
   const Minus = () => {
     dispatch(decrement());
   }
   const Add = () => {
     dispatch(increment());
   }
+//second
+  const MinusSec = () => {
+    dispatch(SecMinus());
+  }
+  const AddSec = () => {
+    dispatch(SecPlus());
+  }
+//Third
+  const MinusTrd = () => {
+    dispatch(minus());
+  }
+  const AddTrd = () => {
+    dispatch(add());
+  }
+  //Reset All
+  const ClearAll =()=>{
+    localStorage.clear();
+    location.reload();
+  }
 
   return (
     <>
       <Box sx={{ textAlign: 'center', mt: 4 }}>
-
+        <Typography variant="body1" color="initial">Frist Slice</Typography>
         <Button sx={{ color: '#000', mr: 4 }} onClick={Minus}>-</Button>
-        <span>{count.value}</span>
+        <span>{count.firstCounter.value}</span>
         <Button sx={{ color: '#000', ml: 4 }} onClick={Add}>+</Button>
+        <br />
+
+        <Box mt={6}>
+          <Typography variant="body1" color="initial">Second Slice</Typography>
+          <Button sx={{ color: '#000', mr: 4 }} onClick={MinusSec}>-</Button>
+          <span>{count.secondCounter.secondNum}</span>
+          <Button sx={{ color: '#000', ml: 4 }} onClick={AddSec}>+</Button>
+          <br />
+        </Box>
+
+        <Box mt={6}>
+          <Typography variant="body1" color="initial">Third Slice</Typography>
+          <Button sx={{ color: '#000', mr: 4 }} onClick={MinusTrd}>-</Button>
+          <span>{count.thirdCounter.ThirdNum}</span>
+          <Button sx={{ color: '#000', ml: 4 }} onClick={AddTrd}>+</Button>
+          <br />
+        </Box>
+        <Button sx={{ color: '#000', mt: 4 }} onClick={ClearAll}>Reset All</Button>
       </Box>
     </>
   )
